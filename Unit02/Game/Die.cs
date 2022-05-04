72d7226ff756931056efb17123cc740015ca452d
Jared Davey <dav15024@byui.edu>
2022-05-04 17:15:56 -0600

    Finished Die specifications, game works well :)

--- Unit02/Game/Die.cs
+++ Unit02/Game/Die.cs
@@ -12,6 +12,12 @@ namespace Unit02.Game
         /// The responsibility of Die is to keep track of its currently rolled value and the points its
         /// worth.
         /// </summary> 
+        public class Die
+        {
+            public int value;
+            public int points;
+
+
 
 
     // 2) Create the class constructor. Use the following method comment.
@@ -19,6 +25,11 @@ namespace Unit02.Game
         /// <summary>
         /// Constructs a new instance of Die.
         /// </summary>
+            public Die()
+            {
+                value = 0;
+                points = -1;
+            } // exit Die constructor.
 
     
     // 3) Create the Roll() method. Use the following method comment.
@@ -27,5 +38,26 @@ namespace Unit02.Game
         /// Generates a new random value and calculates the points for the die. Fives are 
         /// worth 50 points, ones are worth 100 points, everything else is worth 0 points.
         /// </summary>
+            public void Roll()
+            {
+                Random random = new Random();
+                int rNum = random.Next(1, 7);
+                // Thanks to docs.microsoft for teaching me how to
+                // make a random number generator.
+                value = rNum;
+                if (value == 5)
+                {
+                    points = 50;
+                }
+                else if (value == 1)
+                {
+                    points = 100;
+                }
+                else
+                {
+                    points = 0;
+                }
+            }
         
+        } // exit Die class
 }
